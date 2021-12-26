e5eae7850fcd468ff489c6fcf8d18bf92427d936
bozeeetopic <btopic00@fesb.hr>
2021-12-27 00:08:32 +0100

    Merry Christmas and new year to mr reviever

---
+++ InternshipApp/InternshipApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Happy christmas and new year!");
+        }
+    }
+}
