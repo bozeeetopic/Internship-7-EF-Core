3189965a1ded7080567f32046340a9db1a9f1428
bozeeetopic <btopic00@fesb.hr>
2021-12-29 15:56:59 +0100

    Added sus intro animations

---
+++ InternshipApp/Presentation/Helpers/ConsoleHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Presentation.Helpers
+{
+    public class ConsoleHelpers
+    {
+        public static void ClearNumberOfLinesFromConsole(int numberOfLinesToDelete)
+        {
+            for (var i = 0; i < numberOfLinesToDelete; i++)
+            {
+                Console.Write(new string(' ', Console.WindowWidth));
+                Console.SetCursorPosition(0, Console.CursorTop - i);
+            }
+        }
+        public static void WriteInColor(string input, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(input);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
--- InternshipApp/Presentation/Program.cs
+++ InternshipApp/Presentation/Program.cs
@@ -1,10 +1,11 @@
-﻿
+﻿using Presentation.Helpers;
 namespace PaymentManager.Presentation
 {
     public class Program
     {
         static void Main()
         {
+            IntroAnimation.PrintIntro( 100, 300);
         }
     }
 }
